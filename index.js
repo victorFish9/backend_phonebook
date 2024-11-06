b91d30c6ca2e85415b69320414f5668b4aed4da4
Victor <aue228lublupacanov@gmail.com>
2024-11-06 10:47:14 +0100

    start command added

--- index.js
+++ index.js
@@ -106,7 +106,7 @@ app.delete('/api/persons/:id', (request, response) => {
     response.status(200).json({ message: 'Person deleted' })
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
