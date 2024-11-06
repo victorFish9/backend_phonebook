f9d5f7292580792d927db85c2af4f8f687bfcb1d
Victor <aue228lublupacanov@gmail.com>
2024-11-06 11:00:58 +0100

    frontend build added to backend

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const morgan = require('morgan')
 const app = express()
 const cors = require('cors')
 
+
 app.use(cors())
 app.use(express.json())
+app.use(express.static('dist'))
 
 morgan.token('body', (req) => {
     if (req.method === 'POST') {
